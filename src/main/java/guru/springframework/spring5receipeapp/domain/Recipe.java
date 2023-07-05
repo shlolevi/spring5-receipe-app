9947a61edfcdbc8ebd322c93f2289fa46e04f2e8
s.levi <s.levi@proxymity.io>
2023-07-05 16:27:08 +0300

    Add Category Entity

--- src/main/java/guru/springframework/spring5receipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5receipeapp/domain/Recipe.java
@@ -2,6 +2,8 @@ package guru.springframework.spring5receipeapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Recipe {
     @Id
@@ -17,6 +19,12 @@ public class Recipe {
     private Difficulty difficulty;
     @Lob
     private Byte[] image;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -25,9 +33,6 @@ public class Recipe {
     public void setId(Long id) {
         this.id = id;
     }
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private Notes notes;
 
     public String getDescription() {
         return description;
@@ -99,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
